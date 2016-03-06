5d7906b8bac82c16ab110d017449b4156507384d
nibral <nibral@dancingstarson.me>
2016-03-06 20:20:22 +0900

    認証情報を~/.aws/credencialsから読み込むようにした

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-AWS.config.loadFromPath('./config.json');
+AWS.config.region = 'ap-northeast-1';
 
 exports.handler = (event, context) => {
-    
+    // write code here
 }
